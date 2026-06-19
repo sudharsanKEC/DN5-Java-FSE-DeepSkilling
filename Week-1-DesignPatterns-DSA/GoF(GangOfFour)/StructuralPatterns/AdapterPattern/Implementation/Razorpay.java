9ce845d2d7da71694f3d00a8a771e266065018e8
Sudharsan <155707028+sudharsanKEC@users.noreply.github.com>
2026-06-19 22:14:34 +0530

    Create Razorpay.java

---
+++ Week-1-DesignPatterns-DSA/GoF(GangOfFour)/StructuralPatterns/AdapterPattern/Implementation/Razorpay.java
@@ -0,0 +1,9 @@
+// A class from third party library.
+public class Razorpay {
+    public void makePayment(double amount) {
+        System.out.println(
+            "Payment of " + amount +
+            " processed by Razorpay"
+        );
+    }    
+}
