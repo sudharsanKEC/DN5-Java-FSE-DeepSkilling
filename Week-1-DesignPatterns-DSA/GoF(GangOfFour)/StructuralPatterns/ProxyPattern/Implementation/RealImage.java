dcfdc2b03b0475081696ccdbecc722f07206db23
Sudharsan <155707028+sudharsanKEC@users.noreply.github.com>
2026-06-20 15:28:47 +0530

    Implement RealImage class for Proxy Pattern

---
+++ Week-1-DesignPatterns-DSA/GoF(GangOfFour)/StructuralPatterns/ProxyPattern/Implementation/RealImage.java
@@ -0,0 +1,18 @@
+public class RealImage implements Image{
+
+    private String fileName;
+
+    public RealImage(String filename){
+        this.fileName = filename;
+        loadFromDisk();
+    }
+
+    private void loadFromDisk(){
+        System.out.println("Loading image from disk: "+fileName);
+    }
+
+    @Override
+    public void display(){
+        System.out.println("Displaying image: "+fileName);
+    }
+}
