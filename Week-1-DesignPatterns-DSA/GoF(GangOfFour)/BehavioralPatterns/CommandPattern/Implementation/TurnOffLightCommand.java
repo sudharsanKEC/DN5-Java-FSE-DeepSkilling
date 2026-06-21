ee50df30789b99a219db2f2b2e807c8fa90facf1
Sudharsan <155707028+sudharsanKEC@users.noreply.github.com>
2026-06-21 20:30:53 +0530

    Create TurnOffLightCommand.java

---
+++ Week-1-DesignPatterns-DSA/GoF(GangOfFour)/BehavioralPatterns/CommandPattern/Implementation/TurnOffLightCommand.java
@@ -0,0 +1,14 @@
+public class TurnOffLightCommand implements Command{
+    
+    private Light light;
+
+    public TurnOffLightCommand(Light light){
+        this.light = light;
+    }
+
+    @Override
+    public void execute(){
+        light.turnOff();
+    }
+    
+}
