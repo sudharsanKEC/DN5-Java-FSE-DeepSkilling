3aea00857bb9c19eef36736c7d9c0f972f637898
Sudharsan <155707028+sudharsanKEC@users.noreply.github.com>
2026-06-22 10:05:26 +0530

    Create MilkDecorator.java

---
+++ Week-1-DesignPatterns-DSA/GoF(GangOfFour)/StructuralPatterns/DecoratorPattern/Implementation/MilkDecorator.java
@@ -0,0 +1,17 @@
+public class MilkDecorator extends CoffeeDecorator {
+
+    public MilkDecorator(Coffee coffee){
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription(){
+        return coffee.getDescription() + ", Milk";
+    }
+
+    @Override
+    public int getCost() {
+        return coffee.getCost()+10;
+    }
+    
+}
