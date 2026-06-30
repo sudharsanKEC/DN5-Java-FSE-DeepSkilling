2b6d90e7cda3e88d47b0b83f14d75584de0ae70a
Sudharsan <155707028+sudharsanKEC@users.noreply.github.com>
2026-06-30 09:17:51 +0530

    Create TimeComplexities.java

---
+++ Week-1-DesignPatterns-DSA/DSA/LinkedLists/TimeComplexities.java
@@ -0,0 +1,11 @@
+| Operation             |       SLL       |    DLL    |      CSLL       |   CDLL   |
+| --------------------- | :------------:  | :------:  | :------------:  | :------: |
+| Insert at Beginning   |    **O(1)**     | **O(1)**  |    **O(n)**     | **O(1)** |
+| Insert at End         |    **O(n)**     | **O(n)**  |    **O(n)**     | **O(1)** |
+| Insert at Position    |    **O(n)**     | **O(n)**  |    **O(n)**     | **O(n)** |
+| Delete from Beginning |    **O(1)**     | **O(1)**  |    **O(n)**     | **O(1)** |
+| Delete from End       |    **O(n)**     | **O(n)**  |    **O(n)**     | **O(1)** |
+| Delete from Position  |    **O(n)**     | **O(n)**  |    **O(n)**     | **O(n)** |
+| Search                |    **O(n)**     | **O(n)**  |    **O(n)**     | **O(n)** |
+| Display Forward       |    **O(n)**     | **O(n)**  |    **O(n)**     | **O(n)** |
+| Display Reverse       | ❌ Not Possible | **O(n)** | ❌ Not Possible | **O(n)** |
