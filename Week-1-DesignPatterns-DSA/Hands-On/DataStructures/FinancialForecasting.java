f4a5073af58b4f321ef3376163a85d93e678d8ad
Sudharsan <155707028+sudharsanKEC@users.noreply.github.com>
2026-07-08 20:37:02 +0530

    Create FinancialForecasting.java

---
+++ Week-1-DesignPatterns-DSA/Hands-On/DataStructures/FinancialForecasting.java
@@ -0,0 +1,24 @@
+public class FinancialForecast {
+
+    public static double futureValue(double presentValue, double growthRate, int years) {
+
+        // Base case
+        if (years == 0) {
+            return presentValue;
+        }
+
+        // Recursive case
+        return futureValue(presentValue, growthRate, years - 1) * (1 + growthRate);
+    }
+
+    public static void main(String[] args) {
+
+        double presentValue = 10000;
+        double growthRate = 0.10;   // 10%
+        int years = 5;
+
+        double future = futureValue(presentValue, growthRate, years);
+
+        System.out.printf("Future Value after %d years = %.2f", years, future);
+    }
+}
