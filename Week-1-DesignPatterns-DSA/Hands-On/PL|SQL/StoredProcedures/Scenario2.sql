0e2590a97bbca0e22f140982c629b9c20e1d8935
Sudharsan <155707028+sudharsanKEC@users.noreply.github.com>
2026-07-08 21:46:47 +0530

    Add stored procedure to update employee bonuses

---
+++ Week-1-DesignPatterns-DSA/Hands-On/PL|SQL/StoredProcedures/Scenario2.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus(
+    p_department IN VARCHAR2,
+    p_bonus_percent IN NUMBER
+)
+IS
+BEGIN
+
+    UPDATE Employees
+    SET Salary = Salary + (Salary * p_bonus_percent / 100)
+    WHERE Department = p_department;
+
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('Employee bonus updated successfully.');
+
+END;
+/
+
+BEGIN
+    UpdateEmployeeBonus('IT',10);
+END;
+/
