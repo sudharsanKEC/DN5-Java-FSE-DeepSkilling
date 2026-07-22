4743005705bd57986aaef4043b891a14f733abc9
sudharsanKEC <sudharsans913@gmail.com>
2026-07-22 10:06:09 +0530

    Microservice mandatory exercise uploaded

---
+++ Week-4-CodeQuality-SonarQube-Microservices/Microservices-Handson/HandsOn3-Mandatory/api-gateway/src/main/java/com/cognizant/api_gateway/filter/LogFilter.java
@@ -0,0 +1,30 @@
+package com.cognizant.api_gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LogFilter implements GlobalFilter, Ordered {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public Mono<Void> filter(org.springframework.web.server.ServerWebExchange exchange,
+                             org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
+
+        logger.info("Incoming Request : {} {}",
+                exchange.getRequest().getMethod(),
+                exchange.getRequest().getURI());
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+}
---
+++ Week-4-CodeQuality-SonarQube-Microservices/Microservices-Handson/HandsOn3-Mandatory/greet-service/src/test/java/com/cognizant/greetservice/GreetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.greetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
