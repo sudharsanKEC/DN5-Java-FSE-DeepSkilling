404abf40cdd555cf465adda1ba2bd26a27461f12
sudharsanKEC <sudharsans913@gmail.com>
2026-07-23 21:13:30 +0530

    Completed some additional exercises in Spring And Maven

---
+++ Week-2-SpringAndMaven/HandsOn-Exercises/Exercise3/src/main/java/com/library/Main.java
@@ -0,0 +1,28 @@
+package com.library;
+
+import com.library.repository.BookRepository;
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service = context.getBean("bookService", BookService.class);
+
+        service.addBook();
+        service.removeBook();
+    }
+}
+
+/*
+Exercise 1:
+    We added the spring-context dependency, which provides the Spring IoC Container and related classes. In applicationContext.xml, we defined two beans: bookRepository and bookService. When we created a ClassPathXmlApplicationContext and passed it the XML configuration file, Spring read the file, created the bean objects, and started managing them inside the IoC container. Later, when we called context.getBean(), Spring returned references to those already managed bean objects. We then used those references to invoke the methods of the respective classes.
+
+
+
+*/
---
+++ Week-2-SpringAndMaven/HandsOn-Exercises/Exercise3/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,34 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // the method name should exactly match the bookRepository dependency, so that spring will identify it.
+    // Because in the applicationContext.xml: we have <property name="bookRepository" ref="bookRepository"/>, property name = bookRepository, then spring automatically looks for the below pattern.
+    /*
+        Then it automatically looks for a setter whose name follows this pattern:
+            set + Capitalize(first letter of property name)
+                bookRepository
+                    │
+                    ▼
+                setBookRepository()
+        This is called the JavaBean naming convention.
+        The Spring IoC Container calls setBookRepository() automatically while creating the BookService bean, based on the <property> configuration in applicationContext.xml.
+    */
+    public void setBookRepository(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(){
+        System.out.println("BookService: Calling BookRepository...");
+        bookRepository.saveBook();
+    }
+
+    public void removeBook(){
+        System.out.println("BookService: Calling BookRepository...");
+    }
+
+}
